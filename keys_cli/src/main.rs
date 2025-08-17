800df92db48ff43d701d50351c026ea6dac3ce41
JT Raber <JMANN240raber@gmail.com>
2025-08-17 17:20:48 -0400

    licenses and descriptions

--- keys_cli/src/main.rs
+++ keys_cli/src/main.rs
@@ -1,5 +1,7 @@
-use clap::{Parser, Subcommand};
-use dotenvy::dotenv;
+use std::path::PathBuf;
+
+use clap::{Args, Parser, Subcommand};
+use dotenvy::{EnvLoader, EnvSequence};
 use keys_client::KeysClient;
 
 #[derive(Parser)]
@@ -13,26 +15,47 @@ enum Commands {
     Get { key: String },
     Set { key: String, value: String },
     Delete { key: String },
+    Migrate(MigrateArgs),
+}
+
+#[derive(Args)]
+struct MigrateArgs {
+    #[arg(default_value = ".env")]
+    path: PathBuf
 }
 
 #[tokio::main]
 async fn main() {
     let cli = Cli::parse();
-    dotenv().unwrap();
 
     let client = KeysClient::default();
 
-    let key_value = match cli.command {
+    match cli.command {
         Commands::Get { key } => {
-            client.get_value(&key).await
-        }
+            println!("{:?}", client.get_value(&key).await);
+        },
         Commands::Set { key, value } => {
-            client.set_value(&key, &value).await
-        }
+            println!("{:?}", client.set_value(&key, &value).await);
+        },
         Commands::Delete { key } => {
-            client.delete_value(&key).await
+            println!("{:?}", client.delete_value(&key).await);
+        },
+        Commands::Migrate(MigrateArgs { path }) => {
+            let env = EnvLoader::with_path(path).sequence(EnvSequence::InputOnly).load().unwrap();
+
+            for (key, value) in env.iter() {
+                let key_value = client.get_value(&key).await.unwrap();
+
+                match key_value.get_value() {
+                    Some(value) => {
+                        println!("Value '{value}' already exists for key '{key}', not migrating.");
+                    },
+                    None => {
+                        client.set_value(&key, &value).await.unwrap();
+                        println!("Migrated value '{value}' for key '{key}'.");
+                    }
+                }
+            }
         }
     };
-
-    println!("{key_value:?}");
 }
