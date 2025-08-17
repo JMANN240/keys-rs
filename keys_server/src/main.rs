d9fbf1ba292d883357608421f12f7a6bea212e63
JT Raber <JMANN240raber@gmail.com>
2025-08-16 21:53:28 -0400

    yay cors

--- keys_server/src/main.rs
+++ keys_server/src/main.rs
@@ -7,6 +7,7 @@ use clap::{Args, Parser, Subcommand};
 use dotenvy::dotenv;
 use sqlx::SqlitePool;
 use tokio::net::{TcpListener, UnixListener};
+use tower_http::cors::CorsLayer;
 
 mod api;
 mod db;
@@ -90,6 +91,7 @@ where
     let app = axum::Router::<AppState>::new()
         .nest("/key_value", api::key_value::get_router())
         .nest("/api_key", api::api_key::get_router())
+        .layer(CorsLayer::permissive())
         .with_state(state);
 
     axum::serve(listener, app).await.unwrap();
